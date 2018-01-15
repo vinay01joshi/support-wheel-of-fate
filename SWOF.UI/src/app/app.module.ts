6668bfad844ea976158ed5af367c9d9205deb625
Vinay Joshi <vinjoshi@publicisgroupe.net>
2018-01-15 23:15:35 +0530

    Adding Front End Project

---
+++ SWOF.UI/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BsNavbarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ SWOF.UI/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  service: "http://support-wheel-of-fate-swof.7e14.starter-us-west-2.openshiftapps.com/api/"
+};
