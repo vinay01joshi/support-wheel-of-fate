9a9293f20466059ec84fb0984ca9c6e3cc654639
Vinay Joshi <vinjoshi@publicisgroupe.net>
2018-01-23 00:35:40 +0530

    Update Engineer contorller

---
+++ SWOF/Controllers/EngineerController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SWOF.Core.Resources;
+using SWOF.Persistence;
+
+namespace SWOF.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class EngineerController : Controller
+    {
+        private readonly IEngineerRepository _engineerRepository;
+
+        public EngineerController(IEngineerRepository engineerRepository)
+        {
+            _engineerRepository = engineerRepository;
+        }
+
+        /// <summary>
+        /// Retrieves all Engineers
+        /// </summary>
+        /// <returns>List of engineers</returns>
+        [HttpGet]
+        public IEnumerable<EngineerModel> Get()
+        {
+            return _engineerRepository.ReadAll();
+        }
+
+        /// <summary>
+        /// Gets a single engineer with the matching id
+        /// </summary>
+        /// <param name="id">Identifier of the engineer to return</param>
+        /// <returns>The engineer with the specified Id</returns>
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var engineer = _engineerRepository.Find(id);
+            if (engineer == null)
+            {
+                return NotFound();
+            }
+
+            return new ObjectResult(engineer);
+        }
+
+        /// <summary>
+        /// Adds a new engineer with the given name
+        /// </summary>
+        /// <remarks>Note that the name must be quoted if using Swagger</remarks>
+        /// <param name="name">Name of the engineer.</param>
+        /// <returns>Id of the new engineer</returns>
+        [HttpPost]
+        public IActionResult Post([FromBody]string name)
+        {
+            var engineer = new EngineerModel { Name = name };
+            _engineerRepository.Add(engineer);
+            return new ObjectResult(engineer.Id) { StatusCode = StatusCodes.Status201Created };
+        }
+
+        /// <summary>
+        /// Removes the engineer with the given identifier from the repository
+        /// </summary>
+        /// <param name="id">Identifier of the engineer to delete</param>
+        /// <returns>OK if deleted</returns>
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _engineerRepository.Remove(id);
+            return Ok();
+        }
+
+    }
+}
--- SWOF/Persistence/EngineerRepository.cs
+++ SWOF/Persistence/EngineerRepository.cs
@@ -10,27 +10,12 @@ namespace SWOF.Persistence
 {
     public class EngineerRepository : IEngineerRepository
     {
-        private readonly BauDbContext _context;
-        // private DbSet<Engineer> _entities;
-        //private List<Engineer> _entities;
+        private readonly BauDbContext _context;       
         private IMapper _mapper;
 
         public EngineerRepository(BauDbContext context, IMapper mapper)
         {
-            _context = context;
-            //_entities = new List<Engineer>()
-            //{
-            //        new Engineer {Id =1,  Name = "Vinay" },
-            //        new Engineer {Id =2, Name = "Jitender" },
-            //        new Engineer {Id =3, Name = "Shivam" },
-            //        new Engineer {Id =4, Name = "Parvez" },
-            //        new Engineer {Id =5, Name = "Pankaj" },
-            //        new Engineer {Id =6, Name = "Subodh" },
-            //        new Engineer {Id =7, Name = "Peeyush" },
-            //        new Engineer {Id =8, Name = "Harsh" },
-            //        new Engineer {Id =9, Name = "Tanvi" },
-            //        new Engineer {Id =10, Name = "Harry" }
-            //};
+            _context = context;          
             _mapper = mapper;
         }
 
